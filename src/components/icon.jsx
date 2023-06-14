a2953b67946302f3fa9d83f7107d4704745883d3
Arnoldo Magaña <elswordmon@gmail.com>
2023-06-14 17:30:57 -0600

    icons edited

--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -1,7 +1,5 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
-import Icon1 from 'react-native-vector-icons/MaterialIcons'
-import Icon2 from 'react-native-vector-icons/Octicons'
 import Icon3 from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const Icon = ({inmueble}) => {
@@ -9,7 +7,7 @@ const Icon = ({inmueble}) => {
         <View style={styles.row}>
             <Text style={styles.r}><Icon3 name="bed-king-outline" size={20}/>  {inmueble.habitaciones }</Text>
             <Text style={styles.r}><Icon3 name="shower" size={20}/>  {inmueble.banios}</Text>
-            <Text style={styles.r}><Icon3 name="home" size={20}/>  {inmueble.superficie}</Text>
+            <Text style={styles.r}><Icon3 name="vector-square" size={20}/>  {inmueble.superficie}</Text>
         </View>
     );
 };
